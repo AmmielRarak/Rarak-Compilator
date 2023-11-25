2425d00f384e2bc015bb9c7bd6ee3991a2999ffb
Ammiel Rarak <ammielary@gmail.com>
2023-11-25 17:32:38 +0100

    Agregar archivos de proyecto.

---
+++ Rarak-Compilator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Rarak_Compilator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
